f3b6cf29b9804c16a25cdf186532e84e9344cf75
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2024-06-02 13:03:33 +0530

    img

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -18,7 +18,7 @@ const postSchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+        '',
     },
     category: {
       type: String,
